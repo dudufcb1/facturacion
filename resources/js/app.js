3fbd5973329d27936d6ad98f383fb158db517f7a
Eduardo Gonzalez <heyimedd@gmail.com>
2024-11-13 16:52:39 -0600

    Check if alpine is defined, otherwise import it

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+if (typeof Alpine === 'undefined') {
+    import('alpinejs').then((Alpine) => {
+        window.Alpine = Alpine.default
+        Alpine.default.start()
+    })
+}
